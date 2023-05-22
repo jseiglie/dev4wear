7388bbf363f5a13c60feacad9f5a04483f086446
Javier Seiglie <javierseiglie@gmail.com>
2023-05-22 11:14:02 +0200

    changes before reninstalling

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,8 +3,6 @@ import SeparatorComponent from "../components/Separator.component";
 import HookComponent from "../components/Hook.component";
 import LatestComponent from "../components/Latest.component";
 import { Context } from "../state/appContext";
-import { PayPalComponent } from "../components/PayPal.Component";
-
 export const Home = () => {
   const {store, actions} = useContext(Context)
   useEffect(()=>{
